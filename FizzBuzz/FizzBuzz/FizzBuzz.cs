d70e8caff8f4368bc1d71f9de50598c22ca9fcd1
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-29 16:19:31 +0700

    Edit: refactor check condition to new method

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,10 +6,25 @@ namespace FizzBuzz
     {
         public string Say(int number)
         {
-            if (number % 15 == 0) return "FizzBuzz";
-            if (number % 3 == 0) return "Fizz";
-            if (number % 5 == 0) return "Buzz";
+            if (isFizzBuzz(number)) return "FizzBuzz";
+            if (isFizz(number)) return "Fizz";
+            if (isBuzz(number)) return "Buzz";
             return number.ToString();
+        }
+
+        private bool isFizzBuzz(int number)
+        {
+            return number % 15 == 0;
+        }
+
+        private bool isFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private bool isBuzz(int number)
+        {
+            return number % 5 == 0;
         }
     }
 }
